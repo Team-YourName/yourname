5f6622c3fc10c946ec7ec063cebe3d2833e45f27
BOYOUNG-KANG <by28287@gmail.com>
2025-03-20 20:28:16 +0900

    feat : 로딩 화면 생성

---
+++ src/pages/LoadingPage.vue
@@ -0,0 +1,226 @@
+<template>
+    <div class="t-container">
+        <h2 class="title">가장 잘 어울리는 이름을 찾고 있어요!</h2>
+    </div>
+
+        <div class="second-container">
+          <div class="name-circle !left-[-20px] !top-[485px] !w-60 !max-w-[170px] !ml-[-3px]">
+                <p class="name relative">Christine
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[160px] !top-[485px] !w-60 !max-w-[130px] !ml-[-3px]">
+                <p class="name relative">Colin
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[300px] !top-[485px] !w-60 !max-w-[135px] !ml-[-3px]">
+                <p class="name relative">Ellen
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+        </div>
+
+          <div class="container">
+            <div class="name-circle !w-60 !max-w-[150px] !ml-[-3px]">
+                <p class="name relative">Dustin
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !w-40 !max-w-[130px] !ml-[158px]">
+                <p class="name relative">Bella 
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !w-40 !max-w-[130px] !ml-[300px]">
+                <p class="name">David 
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+        </div>
+
+        <div class="second-container">
+            <div class="name-circle !left-[-95px] !top-[623px] !w-60 !max-w-[150px] !ml-[-3px]">
+                <p class="name relative">Sophia
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[65px] !top-[623px] !w-60 !max-w-[140px] !ml-[-3px]">
+                <p class="name relative">Asher
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[213px] !top-[623px] !w-60 !max-w-[115px] !ml-[-3px]">
+                <p class="name relative">Mia
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[335px] !top-[623px] !w-60 !max-w-[142px] !ml-[-3px]">
+                <p class="name relative">Brian
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+        </div>
+
+        <div class="second-container">
+            <div class="name-circle !left-[-18px] !top-[692px] !w-60 !max-w-[140px] !ml-[-3px]">
+                <p class="name relative">Noah
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[130px] !top-[692px] !w-60 !max-w-[140px] !ml-[-3px]">
+                <p class="name relative">John
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[280px] !top-[692px] !w-60 !max-w-[140px] !ml-[-3px]">
+                <p class="name relative">Sarah
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+        </div>
+
+        <div class="second-container">
+            <div class="name-circle !left-[-55px] !top-[762px] !w-60 !max-w-[150px] !ml-[-3px]">
+                <p class="name relative">Emma
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[105px] !top-[762px] !w-60 !max-w-[150px] !ml-[-3px]">
+                <p class="name relative">William
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+
+            <div class="name-circle !left-[265px] !top-[762px] !w-60 !max-w-[147px] !ml-[-3px]">
+                <p class="name relative">Mason
+                    <svg xmlns="http://www.w3.org/2000/svg" class="delete_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </p>
+            </div>
+        </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+@import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@2.0/nanumsquare.css); 
+
+.t-container {
+  max-width: 305px;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 220px;
+}
+
+.title {
+  text-align: center;
+  width: 121;
+  height: 27;
+  top: 119px;
+  left: 136px;
+  font-family: 'NanumSquare', sans-serif;
+  font-weight: 700;
+  size: 20px;
+  font-size: large;
+  color: #252525;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    gap: 50px;
+}
+.second-container {
+    display: flex;
+    justify-content: center;
+    gap: 50px;
+}
+
+.name-circle {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    padding: 15px 28px;
+    position: absolute;
+    left: -23px;
+    bottom: 230px;
+    right: 69%;
+    border: 2px solid #B4B4B4;
+    border-radius: 100px;
+    width: auto; 
+    height: 60px; 
+    align-items: center;
+}
+.name-circle:not(:last-child) {
+    margin-right: 50px;
+}
+
+
+.name {
+    font-family: 'San Francisco', sans-serif;
+    font-weight: 200;
+    font-size: 1.5rem;
+    text-align: center;
+    position: relative;
+    color: #B4B4B4;
+
+    display: flex;
+    margin-top: -5px;
+    gap: 8px;
+    line-height: 1.5;
+}
+
+.delete_icon {    
+    width: 24px; 
+    height: 24px;
+    position: relative;
+    margin-top: 5px;
+    color: #B4B4B4;
+}
+
+
+</style>
